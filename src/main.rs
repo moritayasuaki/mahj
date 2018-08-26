aacc80e2805cc1d71a4704fdd9bde5a10b8b37c4
Yasuaki MORITA <zousandazou@gmail.com>
2018-08-26 22:20:49 +0900

    hello server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Result, Write};
+use std::net::{TcpListener};
+
+fn main() -> Result<()> {
+    println!("binding localhost:8080 ...");
+    let listener = TcpListener::bind("localhost:8080")?;
+    loop {
+        let (mut sock, addr) = listener.accept()?;
+        println!("accepted client {} ", addr);
+        sock.write(b"hello!\n")?;
+        sock.flush()?;
+    }
 }
+
