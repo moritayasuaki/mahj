19b614149fd7aaad5fea8254c9ad8da9f6e03ce8
Yasuaki MORITA <zousandazou@gmail.com>
2018-08-27 10:22:48 +0900

    add channel

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::io;
 use std::net;
 use std::thread;
+use std::sync::mpsc;
 
 use io::Write;
+
+struct Request {
+    thread : u32,
+    req : u32
+}
 
 fn main() -> io::Result<()> {
     println!("binding localhost:8080 ...");
     let listener = net::TcpListener::bind("localhost:8080")?;
     let mut handles = Vec::new();
+    let (tx, rx) = mpsc::channel::<Request>();
     for _ in 0..4 {
         let (mut sock, addr) = listener.accept()?;
         println!("accepted client {} ", addr);
-        let handle = thread::spawn(move || sub(sock, addr));
+        let tx = tx.clone();
+        let handle = thread::spawn(move || sub(sock, addr, tx));
         handles.push(handle);
     }
 
@@ -21,6 +29,6 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn sub(mut sock : net::TcpStream, addr : net::SocketAddr) -> io::Result<()> {
+fn sub(mut sock : net::TcpStream, addr : net::SocketAddr, tx: mpsc::Sender<Request>) -> io::Result<()> {
     writeln!(sock, "hello! {}", addr)
 }
