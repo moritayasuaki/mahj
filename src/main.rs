c21fe63a347f0a0bba0bbf5aaf4437b8d5aef30c
Yasuaki MORITA <zousandazou@gmail.com>
2018-08-27 10:08:25 +0900

    spawn thread

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use std::io::{Result, Write};
-use std::net::{TcpListener};
+use std::io;
+use std::net;
+use std::thread;
 
-fn main() -> Result<()> {
+use io::Write;
+
+fn main() -> io::Result<()> {
     println!("binding localhost:8080 ...");
-    let listener = TcpListener::bind("localhost:8080")?;
-    loop {
+    let listener = net::TcpListener::bind("localhost:8080")?;
+    let mut handles = Vec::new();
+    for _ in 0..4 {
         let (mut sock, addr) = listener.accept()?;
         println!("accepted client {} ", addr);
-        sock.write(b"hello!\n")?;
-        sock.flush()?;
+        let handle = thread::spawn(move || sub(sock, addr));
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join();
+    }
+    Ok(())
 }
 
+fn sub(mut sock : net::TcpStream, addr : net::SocketAddr) -> io::Result<()> {
+    writeln!(sock, "hello! {}", addr)
+}
