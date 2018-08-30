254d7cf86a9afc9d29857a14be2ae025622a2892
Yasuaki MORITA <zousandazou@gmail.com>
2018-08-31 00:08:40 +0900

    bæta Flís Raðtala og viðskipta virka

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,30 @@ use std::ops;
 
 use io::Write;
 
-
+#[derive(Copy,Clone)]
+struct Flís(u8);
 #[derive(Copy,Clone)]
 struct FlísTýpe(u8);
 #[derive(Copy,Clone)]
 struct LiturTýpe(u8);
+#[derive(Copy,Clone)]
+struct Raðtala(u8);
+
+impl Flís {
+    const NÚMER: usize = 136;
+    pub fn auðkenni(self) -> usize {
+        self.0 as usize
+    }
+    pub fn í_flístýpe(self) -> FlísTýpe {
+        FlísTýpe(self.0 / 4)
+    }
+    pub fn frá_auðkenni(au: usize) -> Self {
+        Flís((au % Self::NÚMER) as u8)
+    }
+    pub fn make_iter() -> impl Iterator<Item=Self> {
+        (0..Self::NÚMER).map(Self::frá_auðkenni)
+    }
+}
 
 impl FlísTýpe {
     const NÚMER: usize = 34;
@@ -20,12 +39,12 @@ impl FlísTýpe {
         '🀀','🀁','🀂','🀃','🀄','🀅','🀆','🀇','🀈','🀉','🀊','🀋','🀌','🀍','🀎','🀏',
         '🀐','🀑','🀒','🀓','🀔','🀕','🀖','🀗','🀘','🀙','🀚','🀛','🀜','🀝','🀞','🀟',
         '🀠','🀡'];
-    const VEÐUR_BILINU: ops::Range<usize> = 0..4;
-    const DREKI_BILINU: ops::Range<usize> = 4..7;
-    const HEIÐUR_BILINU: ops::Range<usize> = 0..7;
-    const MYNT_BILINU: ops::Range<usize> = 7..16;
-    const BAMBUS_BILINU: ops::Range<usize> = 16..25;
-    const HRINGUR_BILINU: ops::Range<usize> = 25..34;
+    const _VEÐUR_BILINU: ops::Range<usize> = 0..4;
+    const _DREKI_BILINU: ops::Range<usize> = 4..7;
+    const _HEIÐUR_BILINU: ops::Range<usize> = 0..7;
+    const _MYNT_BILINU: ops::Range<usize> = 7..16;
+    const _BAMBUS_BILINU: ops::Range<usize> = 16..25;
+    const _HRINGUR_BILINU: ops::Range<usize> = 25..34;
 
     pub fn auðkenni(self) -> usize {
         self.0 as usize
@@ -42,7 +61,16 @@ impl FlísTýpe {
         7...15 => LiturTýpe(1),
         16...24 => LiturTýpe(2),
         25...33 => LiturTýpe(3),
-        _ => panic!("Ógilt auðkenni")
+        _ => unreachable!()
+        }
+    }
+    pub fn í_raðtala(self) -> LiturTýpe {
+        match self.auðkenni() {
+        0...6 => LiturTýpe(0),
+        7...15 => LiturTýpe(1),
+        16...24 => LiturTýpe(2),
+        25...33 => LiturTýpe(3),
+        _ => unreachable!()
         }
     }
     pub fn er_veður(self) {
@@ -65,6 +93,25 @@ impl LiturTýpe {
     }
 }
 
+impl Raðtala {
+    const NÚMER: usize = 9;
+    pub fn auðkenni(self) -> usize {
+        self.0 as usize
+    }
+    pub fn frá_auðkenni(au: usize) -> Self {
+        Raðtala((au % Self::NÚMER) as u8)
+    }
+    pub fn er_endastöð(self) -> bool {
+        match self.auðkenni() {
+        0 => true,
+        8 => true,
+        _ => false
+        }
+    }
+    pub fn er_einfalt(self) -> bool {
+        !self.er_endastöð()
+    }
+}
 
 struct Request {
     thread: u32,
@@ -90,6 +137,6 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn sub(mut sock : net::TcpStream, addr : net::SocketAddr, tx: mpsc::Sender<Request>) -> io::Result<()> {
+fn sub(mut sock : net::TcpStream, addr : net::SocketAddr, _tx: mpsc::Sender<Request>) -> io::Result<()> {
     writeln!(sock, "hello! {} {}", FlísTýpe(1).í_letur(), addr)
 }
