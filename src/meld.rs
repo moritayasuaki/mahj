7c3a122158ed1f447dfca1948f42379c2daffaab
Yasuaki MORITA <zousandazou@gmail.com>
2018-09-27 11:14:37 +0900

    fix build error

--- src/meld.rs
+++ src/meld.rs
@@ -13,32 +13,36 @@ pub struct Meld {
 
 
 impl SuitRanks {
+    pub fn tile_count(&self) -> usize {
+        self.ranks.count()
+    }
+
     pub fn is_chow(&self) -> bool {
         self.suit.is_numeric() && self.ranks.count() == 3 && !self.ranks.filter_chow().is_empty()
     }
 
     pub fn is_pong(&self) -> bool {
-        self.ranks.count() == 3 && !self.ranks.filter_chow().is_empty()
+        self.tile_count() == 3 && !self.ranks.filter_chow().is_empty()
     }
 
     pub fn is_kong(&self) -> bool {
-        self.suit.count() == 3 && !self.ranks.filter_chow().is_empty()
+        self.tile_count() == 3 && !self.ranks.filter_chow().is_empty()
     }
 
     pub fn is_kanchan(&self) -> bool {
-        self.suit.count() == 2 && !self.ranks.filter_kanchan().is_empty()
+        self.tile_count() == 2 && !self.ranks.filter_kanchan().is_empty()
     }
 
     pub fn is_penryan(&self) -> bool {
-        self.suit.count() == 2 && !self.ranks.filter_penryan().is_empty()
+        self.tile_count() == 2 && !self.ranks.filter_penryan().is_empty()
     }
 
     pub fn is_penchan(&self) -> bool {
-        self.suit.count() == 2 && !self.ranks.filter_penchan().is_empty()
+        self.tile_count() == 2 && !self.ranks.filter_penchan().is_empty()
     }
 
     pub fn is_ryanmen(&self) -> bool {
-        self.suit.count() == 2 && !self.ranks.filter_ryanmen().is_empty()
+        self.tile_count() == 2 && !self.ranks.filter_ryanmen().is_empty()
     }
 }
 
