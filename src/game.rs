49288fd3958eae2a64da66aba5f8448d2ca7ca68
Yasuaki MORITA <zousandazou@gmail.com>
2018-09-30 01:29:52 +0900

    game phase

--- src/game.rs
+++ src/game.rs
@@ -1,63 +1,222 @@
 use rand;
+use tile::*;
 use table::*;
 use failure;
 
-type Res<T> = Result<T, failure::Error>;
+const OK: Result<(), failure::Error> = Ok(());
 
-struct Game {
-    score: [usize; 4],
-    riichi_sticks: usize,
-    deposit_sticks: usize,
+pub struct Sticks {
+    pub score: [isize; 4],
+    pub burst_flags: u8,
+    pub deposit: isize,
+    pub stack: isize
 }
 
-type Re = Res<()>;
-const OK: Res<()> = Ok(());
+impl Sticks {
+    pub fn new() -> Self {
+        Sticks {
+            score: [25000; 4],
+            burst_flags: 0,
+            deposit: 0,
+            stack: 0
+        }
+    }
+    pub fn payment(&mut self, payer: usize, receiver: usize, points: isize) {
+        self.score[payer] -=  points;
+        self.score[receiver] += points;
+    }
+    pub fn is_bursted(&self) -> bool {
+        self.burst_flags != 0
+    }
+}
 
-impl Game {
-    pub fn run_match(&mut self) -> Re {
-        for prevailing_wind in 0..2 {
-            self.run_round(prevailing_wind)?;
+pub struct State<'a> {
+    table: &'a mut Table,
+    sticks: &'a mut Sticks,
+    round: Wind,
+    dice: (usize, usize),
+    dealer: usize
+}
+
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
+pub enum Phase {
+    Draw(Wind),
+    DrawRidge(Wind),
+    ChooseDrawAction(Wind, Tile),
+    ChooseClaimAction(RiverRef),
+    ChooseDiscardTile(Wind),
+    ChooseRobbingKong(RiverRef),
+}
+
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
+pub enum Finish {
+    WinBySelfDraw(Wind, Tile),
+    WinByDiscard(Wind, RiverRef),
+    ExaustiveDraw,
+    FourRiichiAbort,
+    NineTerminalAbort,
+    FourWindAbort,
+    ThreeWinAbort,
+    FourKongAbort,   
+}
+
+pub fn run_match() -> Result<[isize; 4], failure::Error> {
+    let sticks = &mut Sticks::new();
+    for round in Wind::make_iter() {
+        run_round(sticks, round)?;
+        if sticks.is_bursted() {
+            break;
         }
-        OK
     }
-    pub fn run_half_match(&mut self) -> Re {
-        for prevailing_wind in 0..2 {
-            self.run_round(prevailing_wind)?;
+    Ok(sticks.score)
+}
+
+pub fn run_halfmatch() -> Result<[isize; 4], failure::Error> {
+    let sticks = &mut Sticks::new();
+    for round in Wind::make_iter().take(Wind::N/2) {
+        run_round(sticks, round)?;
+        if sticks.is_bursted() {
+            break;
         }
-        OK
     }
-    pub fn run_round(&mut self, prevailing_wind: Wind) -> Re {
-        for dealer in 0..4 {
-            self.run_hands(prevailing_wind, dealer)?;
+    Ok(sticks.score)
+}
+
+pub fn run_eastmatch() -> Result<[isize; 4], failure::Error> {
+    let sticks = &mut Sticks::new();
+    let round = Wind::from_id(0);
+    run_round(sticks, round)?;
+    Ok(sticks.score)
+}
+
+pub fn run_round(sticks: &mut Sticks, round: Wind) -> Result<(), failure::Error> {
+    for dealer in 0..4 {
+        run_hands(sticks, round, dealer)?;
+        if sticks.is_bursted() {
+            break;
         }
-        OK
     }
-    pub fn run_hands(&mut self, prevailing_wind: Wind, dealer: usize) -> Re{
-        loop {
-            let winner = self.run_hand(prevailing_wind, dealer)?;
-            if winner != dealer {
-                break;
+    Ok(())
+}
+
+pub fn run_hands(sticks: &mut Sticks, round: Wind, dealer: usize) -> Result<(), failure::Error> {
+    let table = &mut Table::new();
+    loop {
+        let bonus_hand = run_hand(sticks, round, dealer, table)?;
+        if sticks.is_bursted() {
+            break;
+        }
+        if !bonus_hand {
+            break;
+        }
+    }
+    Ok(())
+}
+
+pub fn init_table(table: &mut Table, dice: (usize, usize)) {
+    table.shuffle_tiles();
+    table.break_tiles(dice.0 + dice.1);
+    for _ in 0..3 {
+        for seat in Wind::make_iter() {
+            for _ in 0..4 {
+                let tile = table.wall.draw().expect("initial draw");
+                table.lands.get_mut(seat).tiles.add(tile);
             }
         }
-        OK
     }
-    pub fn run_hand(&mut self, prevailing_wind: Wind, dealer: usize) -> Res<usize> {
-        let mut table = Table::new();
-        table.shuffle_tiles();
-        let dice: usize = rand::random();
-        table.break_tiles(dice);
-        for _ in 0..3 {
-            for player in 0..4 {
-                for _ in 0..4 {
-                    let tile = table.wall.draw().expect("initial draw");
-                    table.lands[player].tiles.add(tile);
-                }
-            }
-        }
-        for player in 0..4 {
-            let tile = table.wall.draw().expect("initial draw");
-            table.lands[player].tiles.add(tile);
-        }
-        Ok(0)
+    for seat in Wind::make_iter() {
+        let tile = table.wall.draw().expect("initial draw");
+        table.lands.get_mut(seat).tiles.add(tile);
     }
+}
+
+pub fn run_hand(sticks: &mut Sticks, round: Wind, dealer: usize, table: &mut Table) -> Result<bool, failure::Error> {
+    let dice = (shuffle_dice(), shuffle_dice());
+    init_table(table, dice);
+    let mut phase = Phase::new();
+    let state = &mut State {
+        dice,
+        sticks,
+        round,
+        dealer,
+        table,
+    };
+    let finish = loop {
+        match phase.step(state)? {
+            Ok(next_phase) => phase = next_phase,
+            Err(finish) => break finish,
+        }
+    };
+    finish.payment(state);
+    Ok(finish.has_bonus_hand())
+}
+
+impl Finish {
+    pub fn is_goulash_hand(self) -> bool {
+        use self::Finish::*;
+        match self {
+            ExaustiveDraw | FourRiichiAbort |
+            NineTerminalAbort | FourWindAbort |
+            ThreeWinAbort | FourKongAbort => true,
+            _ => false
+        }
+    }
+    pub fn is_dealer_win(self) -> bool {
+        use self::Finish::*;
+        const EAST: Wind = Wind(0);
+        match self {
+            WinBySelfDraw(EAST, ..) | WinByDiscard(EAST, ..) => true,
+            _ => false
+        }
+    }
+    pub fn has_bonus_hand(self) -> bool {
+        self.is_goulash_hand() || self.is_dealer_win()
+    }
+    pub fn payment(self, state: &mut State) {
+        let _ = state;
+    }
+}
+
+impl Phase {
+    pub fn new() -> Phase {
+        Phase::Draw(EAST)
+    }
+
+    pub fn step<'a>(self, state: &mut State<'a>) -> Result<Result<Self, Finish>, failure::Error> {
+        use self::Phase::*;
+        match self {
+            Draw(seat) => Self::draw(seat, state),
+            DrawRidge(seat) => Self::draw_ridge(seat, state),
+            _ => unimplemented!()
+        }
+    }
+    pub fn draw(seat: Wind, state: &mut State) -> Result<Result<Self, Finish>, failure::Error> {
+        use self::Phase::*;
+        if let Some(tile) = state.table.wall.draw() {
+            Ok(Ok(ChooseDrawAction(seat, tile)))
+        } else {
+            Ok(Err(Finish::ExaustiveDraw))
+        }
+    }
+    pub fn draw_ridge(seat: Wind, state: &mut State) -> Result<Result<Self, Finish>, failure::Error> {
+        use self::Phase::*;
+        if let Some(tile) = state.table.wall.draw_ridge() {
+            Ok(Ok(ChooseDrawAction(seat, tile)))
+        } else {
+            Ok(Err(Finish::ExaustiveDraw))
+        }
+    }
+
+}
+
+pub fn shuffle_dice() -> usize {
+    let r: usize = rand::random();
+    (r % 6) + 1
+}
+
+
+enum DrawAction {
+    Discard(Tile),
+    Kong(Tile),
+    AddingKong(Tile),
 }
--- src/table.rs
+++ src/table.rs
@@ -4,12 +4,15 @@ use rand;
 use mem;
 use std;
 
-pub type Wind = u8;
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
+pub struct Wind(pub u8);
+
+pub const EAST: Wind = Wind(0);
 
 pub struct Table {
     pub wall: Wall,
-    pub lands: [Land; 4],
-    pub rivers: [River; 4]
+    pub lands: Lands,
+    pub rivers: Rivers
 }
 
 pub struct Wall {
@@ -23,24 +26,112 @@ pub struct Land {
     pub melds: Vec<Meld>
 }
 
+#[derive(Copy,Clone,Debug,PartialEq,Eq)]
+pub struct DiscardedTile(u16);
+impl DiscardedTile {
+    // [0..2]: discarded_by
+    // [2..3]: riichi_flag
+    // [3..5]: robbed_by
+    // [5..6]: robbed_flag
+    // [8..16]: tile
+    fn raw(self) -> usize {
+        self.0 as usize
+    }
+    pub fn from_raw(raw: usize) -> Self {
+        DiscardedTile(raw as u16)
+    }
+    pub fn from_tile_seat(tile: Tile, seat: Wind) -> Self {
+        Self::from_raw((tile.id() << 8) | seat.id())
+    }
+    pub fn discarded_by(self) -> Wind {
+        Wind::from_id(self.raw() & 0o3)
+    }
+    pub fn add_riichi_flag(self) -> Self {
+        Self::from_raw(self.raw() | 0o4)
+    }
+    pub fn is_riichi_declaration(self) -> bool {
+        (self.raw() & 0o4) != 0
+    }
+    pub fn add_robbed_mark(self, robbed_by: Wind) -> Self {
+        Self::from_raw(self.raw() | (robbed_by.id() << 3) | 0o40)
+    }
+    pub fn is_robbed(self) -> bool {
+        (self.raw() & 0o40) != 0
+    }
+    pub fn robbed_by(self) -> Option<Wind> {
+        if self.is_robbed() {
+            Some(Wind::from_id((self.raw() >> 3) & 0o3))
+        } else {
+            None
+        }
+    }
+    pub fn tile(self) -> Tile {
+        Tile::from_id(self.raw() >> 8)
+    }
+}
+
+pub struct Rivers2 {
+    pub tiles: [DiscardedTile; 90],
+    pub index: usize
+}
+
+impl Rivers2 {
+    pub fn new() -> Self {
+        Rivers2 {
+            tiles: [DiscardedTile::from_raw(0usize); 90],
+            index: 0
+        }
+    }
+    pub fn get_vec(&self, seat: Wind) -> Vec<DiscardedTile> {
+        self.tiles.iter().filter(|dtile| dtile.discarded_by() == seat).cloned().collect()
+    }
+    pub fn clear(&mut self) {
+        self.index = 0
+    }
+}
+
 pub struct River {
     pub tiles: Vec<Tile>,
     pub riichi: usize,
+}
+
+pub struct Lands([Land; Wind::N]);
+pub struct Rivers([River; Wind::N]);
+
+
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
+pub struct RiverRef(u16);
+
+impl Wind {
+    pub const N: usize = 4;
+    pub fn id(self) -> usize {
+        self.0 as usize
+    }
+    pub fn from_id(id: usize) -> Self {
+        Wind((id % 4) as u8)
+    }
+    pub fn make_iter() -> impl Iterator<Item=Self> {
+        (0..Self::N).map(Self::from_id)
+    }
+    pub fn next(self) -> Self {
+        Self::from_id(self.id() + 1)
+    }
+    pub fn prev(self) -> Self {
+        Self::from_id(self.id() + 3)
+    }
 }
 
 impl Table {
     pub fn new() -> Self {
         Table {
             wall : Wall::new(),
-            lands : [Land::new(), Land::new(), Land::new(), Land::new()],
-            rivers : [River::new(), River::new(), River::new(), River::new()]
+            lands : Lands::new(),
+            rivers : Rivers::new()
         }
     }
     pub fn shuffle_tiles(&mut self) {
-        for i in 0..4 {
-            self.lands[i].clear();
-            self.rivers[i].clear();
-        }
+        self.lands.clear();
+        self.rivers.clear();
         self.wall.shuffle();
     }
     pub fn break_tiles(&mut self, dice: usize) {
@@ -152,16 +243,48 @@ impl River {
     }
 }
 
-pub struct RiverRef(u16);
-
-impl RiverRef {
-    pub fn make(wind: usize, index: usize) -> RiverRef {
-        RiverRef((wind + index * 4) as u16)
+impl Lands {
+    pub fn new() -> Self {
+        Lands([Land::new(), Land::new(), Land::new(), Land::new()])
     }
-    pub fn index(&self) -> usize {
-        (self.0 / 4) as usize
+    pub fn get_mut(&mut self, seat: Wind) -> &mut Land {
+        &mut (self.0)[seat.id()]
     }
-    pub fn wind(&self) -> usize {
-        (self.0 % 4) as usize
+    pub fn get(&self, seat: Wind) -> &Land {
+        &(self.0)[seat.id()]
+    }
+    pub fn clear(&mut self) {
+        self.0.iter_mut().for_each(Land::clear)
     }
 }
+
+impl Rivers {
+    pub fn new() -> Self {
+        Rivers([River::new(), River::new(), River::new(), River::new()])
+    }
+    pub fn get_mut(&mut self, seat: Wind) -> &mut River {
+        &mut (self.0)[seat.id()]
+    }
+    pub fn get(&self, seat: Wind) -> &River {
+        &(self.0)[seat.id()]
+    }
+    pub fn clear(&mut self) {
+        self.0.iter_mut().for_each(River::clear)
+    }
+}
+
+impl RiverRef {
+    pub fn raw(self) -> usize {
+        self.0 as usize
+    }
+    pub fn make(wind: usize, index: usize) -> RiverRef {
+        RiverRef((wind + index * Wind::N) as u16)
+    }
+    pub fn index(self) -> usize {
+        (self.raw() / Wind::N)
+    }
+    pub fn wind(self) -> Wind {
+        Wind::from_id(self.raw())
+    }
+}
+
